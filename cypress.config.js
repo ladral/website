45cf6d5564735db95a2bfe70441d6d7ee4037cba
ladral <tuor_philipp@hotmail.com>
2022-08-07 10:06:50 +0100

    test: adjust folder structure

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
-const { defineConfig } = require("cypress");
+const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    fixturesFolder: "test/cypress/fixtures",
+    screenshotsFolder: "test/cypress/screenshots",
+    videosFolder: "test/cypress/videos",
+    downloadsFolder: "test/cypress/downloads",
+    e2e: {
+        supportFile: "test/cypress/support/e2e.js",
+        specPattern: "test/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+
+        },
     },
-  },
 });
