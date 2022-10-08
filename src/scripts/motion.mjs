d45475db22962344b74d2d5e8e3f6d5978f2c097
ladral <tuor_philipp@hotmail.com>
2022-10-08 23:12:47 +0100

    feat: enhance headline animation

--- src/scripts/motion.mjs
+++ src/scripts/motion.mjs
@@ -1,13 +1,18 @@
-import { animate, inView } from "motion"
+import {timeline, inView} from "motion"
 
-const headlines = document.querySelectorAll(".headline");
+const headlines = document.querySelectorAll(".headline--animated");
 
 headlines.forEach(headline => {
+    const heading = headline.querySelector(".headline__text")
+    const underscore = headline.querySelector(".headline__underscore")
+
+    const sequence = [
+        [heading, {opacity: 1, transform: "none"}, {duration: 0.5, easing: [0.17, 0.55, 0.55, 1]}],
+        [underscore, {width: "100%", transform: "none"}, {duration: 1, easing: [0.17, 0.55, 0.55, 1]}]
+    ]
+
     inView(headline, () => {
-        animate(
-            headline,
-            { opacity: 1, transform: "none" },
-            { delay: 0.5, duration: 0.9, easing: [0.17, 0.55, 0.55, 1] }
-        );
+        timeline(sequence, {delay: 0.5})
+
     });
 })
