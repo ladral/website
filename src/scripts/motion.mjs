c9308280548838e0cbf88d2fc0f75399591b9830
ladral <tuor_philipp@hotmail.com>
2022-10-09 13:54:41 +0100

    feat: enhance headline animation

--- src/scripts/motion.mjs
+++ src/scripts/motion.mjs
@@ -7,12 +7,11 @@ headlines.forEach(headline => {
     const underscore = headline.querySelector(".headline__underscore")
 
     const sequence = [
-        [heading, {opacity: 1, transform: "none"}, {duration: 0.5, easing: [0.17, 0.55, 0.55, 1]}],
+        [heading, {opacity: 1, clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)", transform: "none"}, {duration: 1, easing: [0.17, 0.55, 0.55, 1]}],
         [underscore, {width: "100%", transform: "none"}, {duration: 1, easing: [0.17, 0.55, 0.55, 1]}]
     ]
 
     inView(headline, () => {
         timeline(sequence, {delay: 0.5})
-
     });
 })
