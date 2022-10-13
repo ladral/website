afd9de27b5670dd3f01f881488d4eb5b309f5cd5
ladral <tuor_philipp@hotmail.com>
2022-10-13 20:47:37 +0100

    feat: add scroll hint animation

--- src/scripts/motion.mjs
+++ src/scripts/motion.mjs
@@ -1,4 +1,4 @@
-import {timeline, inView} from "motion"
+import {timeline, inView, animate, scroll} from "motion"
 
 const headlines = document.querySelectorAll(".headline--animated");
 
@@ -7,11 +7,42 @@ headlines.forEach(headline => {
     const underscore = headline.querySelector(".headline__underscore")
 
     const sequence = [
-        [heading, {opacity: 1, clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)", transform: "none"}, {duration: 1, easing: [0.17, 0.55, 0.55, 1]}],
-        [underscore, {width: "100%", transform: "none"}, {duration: 1, easing: [0.17, 0.55, 0.55, 1]}]
+        [heading,
+            {opacity: 1, clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)", transform: "none"},
+            {duration: 1, easing: [0.17, 0.55, 0.55, 1]}
+        ],
+        [underscore,
+            {width: "100%", transform: "none"}, {duration: 1, easing: [0.17, 0.55, 0.55, 1]}
+        ]
     ]
 
     inView(headline, () => {
-        timeline(sequence, {delay: 0.5})
+        const controls = timeline(sequence, {delay: 0.5})
+        
+        return (leaveInfo) => {
+            //TODO: reset animation only if element leaves the window on the upper edge
+            controls.currentTime = 0
+            controls.stop()
+        }
     });
 })
+
+const scrollHintArrows = document.querySelectorAll(".illustration__scroll-hint-arrow");
+
+scrollHintArrows.forEach((arrow, index) => {
+    animate(
+        arrow,
+        {y: [0, 10 + (index * 2), 0]},
+        {easing: ["ease-in-out", "ease", "ease-in-out"], repeat: Infinity, duration: 1.5}
+    )
+})
+
+const scrollHint = document.querySelector(".illustration__scroll-hint")
+
+scroll(({y}) => {
+        const scrollAnimationDistance = 100;
+        let currentScrollPositionY = y.current;
+
+        scrollHint.style.setProperty('--__illustration__scroll-hint-Opacity', (1 - (currentScrollPositionY / scrollAnimationDistance)));
+    }
+)
