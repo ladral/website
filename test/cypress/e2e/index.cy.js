c6b5241da56564b3fc96a33a066c64364e98f1ca
ladral <tuor_philipp@hotmail.com>
2022-10-17 20:54:36 +0200

    test(e2e): add tests for scroll hint motion

--- test/cypress/e2e/index.cy.js
+++ test/cypress/e2e/index.cy.js
@@ -6,16 +6,33 @@ describe('When navigate to index page', () => {
                 .find('link[rel="icon"]')
                 .should('have.attr', 'href')
                 .should('contain', "favicon-");
-        })
-    })
+        });
+    });
 
-    context("svg elements", () => {
+    context('svg elements', () => {
         it('should have viewbox attribute', () => {
             cy.get('svg')
                 .each((item, index) => {
                     cy.wrap(item)
                         .should('have.attr', 'viewBox')
-                })
-        })
-    })
+                });
+        });
+    });
+
+    context('scroll hint', () => {
+        it('should be visible on page load', () => {
+            cy.get('.illustration__scroll-hint').each(item => {
+                cy.wrap(item)
+                    .should('be.visible');
+            });
+        });
+
+        it('should not be visible after scrolling down', () => {
+            cy.get('.illustration__scroll-hint').each(item => {
+                cy.scrollTo(0, 100)
+                cy.wrap(item)
+                    .should('not.be.visible')
+            });
+        });
+    });
 })
