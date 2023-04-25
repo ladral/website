84865ff06705b2181f0456a76ead2ff4baae0311
ladral <tuor_philipp@hotmail.com>
2023-04-25 22:27:43 +0200

    feat: use alpine state to include images in splide

--- src/scripts/state/modal.mjs
+++ src/scripts/state/modal.mjs
@@ -3,23 +3,33 @@ import '../../../node_modules/@splidejs/splide/dist/css/splide-core.min.css';
 import Splide from '@splidejs/splide';
 
 let splide = new Splide( '.splide', {pagination: true}).mount();
-
 document.addEventListener('alpine:init', () => {
     Alpine.data('modal', () => ({
         state: {
             open: false
         },
         project: {
-            title: ""
+            title: "",
+            images: [
+                {
+                    id: "splied01-slide01",
+                    url: new URL('../../assets/images/ladral-website-cloud.png', import.meta.url),
+                },
+                {
+                    id: "splied01-slide02",
+                    url: new URL('../../assets/images/ladral-website-cloud.png', import.meta.url),
+                }
+            ]
         },
 
         setModal(title)  {
-            splide.go(0);
             this.state.open = true;
             this.project.title = title;
 
             let page = document.querySelector(".page")
             page.classList.toggle('u-no-scroll', true);
+            splide.go(0);
+            splide.refresh();
         },
         closeModal() {
             this.state.open = false;
