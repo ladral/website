c58ea252faf16328389da10e75d003b2f0277890
ladral <tuor_philipp@hotmail.com>
2023-04-30 22:38:57 +0200

    feat: handle images for multiple projects

--- src/scripts/state/modal.mjs
+++ src/scripts/state/modal.mjs
@@ -1,41 +1,61 @@
 import '../../../node_modules/@splidejs/splide/dist/css/themes/splide-default.min.css';
 import '../../../node_modules/@splidejs/splide/dist/css/splide-core.min.css';
 import Splide from '@splidejs/splide';
+let gallery = new Splide('.splide').mount();
 
-let splide = new Splide( '.splide', {pagination: true}).mount();
 document.addEventListener('alpine:init', () => {
     Alpine.data('modal', () => ({
         state: {
             open: false
         },
-        project: {
-            title: "",
-            images: [
-                {
-                    id: "splied01-slide01",
-                    url: new URL('../../assets/images/ladral-website-cloud.png', import.meta.url),
-                },
-                {
-                    id: "splied01-slide02",
-                    url: new URL('../../assets/images/ladral-website-cloud.png', import.meta.url),
-                }
-            ]
-        },
+        images: [],
+        projects: [
+            {
+                title: 'Ladral Website',
+                images: [
+                    {
+                        id: "splied01-slide01",
+                        url: new URL('../../assets/images/ladral-website-cloud.png', import.meta.url),
+                    }
+                ]
+            },
+            {
+                title: 'Assembly Dashboard',
+                images: [
+                    {
+                        id: "splied01-slide01",
+                        url: new URL('../../assets/images/ladral-website-cloud.png', import.meta.url),
+                    },
+                    {
+                        id: "splied01-slide02",
+                        url: new URL('../../assets/images/ladral-website-cloud.png', import.meta.url),
+                    }
+                ]
+            }
+        ],
 
-        setModal(title)  {
-            this.state.open = true;
-            this.project.title = title;
+        setModal(title) {
+            // get images for matching project title
+            // an update of the images will trigger a gallery refresh -> see x-init watching for images
+            this.images = this.projects.filter(project => project.title === title).map(project => project.images)[0];
+
+            // navigate gallery to first image
+            gallery.go(0);
 
             let page = document.querySelector(".page")
             page.classList.toggle('u-no-scroll', true);
-            splide.go(0);
-            splide.refresh();
+            this.state.open = true;
         },
         closeModal() {
             this.state.open = false;
 
             let page = document.querySelector(".page")
             page.classList.toggle('u-no-scroll', false);
+        },
+        refreshGallery() {
+            // refresh gallery to handle updated content -> splide.js does not automatically refresh if content changes
+            gallery.refresh();
+            gallery.go(0);
         }
     }))
 })
